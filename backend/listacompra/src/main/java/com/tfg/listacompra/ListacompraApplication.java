1a9e41a35aee1181013fd836622902dc23a6be1d
JsLaveda <lavedasjesus@gmail.com>
2025-10-03 12:04:57 +0200

    Inicio-Springboot

---
+++ backend/listacompra/src/main/java/com/tfg/listacompra/ListacompraApplication.java
@@ -0,0 +1,13 @@
+package com.tfg.listacompra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListacompraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListacompraApplication.class, args);
+	}
+
+}
---
+++ backend/listacompra/src/test/java/com/tfg/listacompra/ListacompraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfg.listacompra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListacompraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
