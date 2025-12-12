4f395552b974052dc9d04471949405a3d785dc67
Mari-Martinez-Rondan <441451@alu.murciaeduca.es>
2025-12-12 03:40:46 +0100

    usar environment.apiUrl en services

--- frontend/src/app/core/services/lista-api.service.ts
+++ frontend/src/app/core/services/lista-api.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ListaApiService {
-  private apiUrl = 'http://localhost:8080/api/listas';
+  private apiUrl = `${environment.apiUrl}/listas`;
     constructor(private http: HttpClient) {}
 
     // Obtener listas del usuario autenticado
--- frontend/src/app/core/services/perfil.service.ts
+++ frontend/src/app/core/services/perfil.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PerfilService {
-  private apiUrl = 'http://localhost:8080/api/perfil';
+  private apiUrl = `${environment.apiUrl}/perfil`;
     constructor(private http: HttpClient) {}
 
     //Metodo para obtener el perfil del usuario
