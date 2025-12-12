00238eef326119a0d2b78b21a233e049e341df3c
Mari-Martinez-Rondan <441451@alu.murciaeduca.es>
2025-12-12 05:22:07 +0100

    modificación cors

--- frontend/src/app/environments/environment.ts
+++ frontend/src/app/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
 	production: false,
 	apiUrl: 'http://localhost:8080/api',
 	// Habilitar el uso de datos simulados (mocks) desde json-server en localhost
-	useMocks: true
+	useMocks: false
 };
